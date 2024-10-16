625319d8f11b900e3ed2d4f6c55e1c26ffaa030b
Anwar30174 <anwaralisarbudeen@gmail.com>
2024-10-16 11:19:15 +0400

    Create emi.js

---
+++ emi.js
@@ -0,0 +1,9 @@
+function calculateEMI() {
+    const loanAmount = parseFloat(document.getElementById('loanAmount').value);
+    const interestRate = parseFloat(document.getElementById('interestRate').value) / 100 / 12;
+    const loanTerm = parseFloat(document.getElementById('loanTerm').value);
+    
+    const monthlyPayment = (loanAmount * interestRate * Math.pow(1 + interestRate, loanTerm)) / (Math.pow(1 + interestRate, loanTerm) - 1);
+    
+    document.getElementById('result').innerHTML = `Monthly EMI: ₹${monthlyPayment.toFixed(2)}`;
+  }
